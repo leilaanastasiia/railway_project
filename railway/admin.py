53f922239275e283ca97d0504ae52652d142cf8b
leilaanastasiia <karpets.leilaanastasia@gmail.com>
2024-10-15 18:25:08 +0300

    relationships

--- railway/admin.py
+++ railway/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from railway.models import RailwayStation, Train, Route
+from railway.models import RailwayStation, Route, Train, CustomUser, Ticket
 
-admin.site.register(RailwayStation)
-admin.site.register(Train)
-admin.site.register(Route)
+
+class RailwayStationAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    filter_horizontal = ('stations',)
+
+
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('number', 'type', 'route', 'station')
+    list_filter = ('type', 'route', 'station')
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'train', 'start_station', 'end_station', 'user')
+    list_filter = ('train', 'start_station', 'end_station')
+
+
+admin.site.register(RailwayStation, RailwayStationAdmin)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Train, TrainAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(CustomUser)
--- railway/models.py
+++ railway/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(unique=True, blank=False)
+
+    def __str__(self):
+        return f'id={self.id}, username={self.username}, email={self.email}'
 
 
 class RailwayStation(models.Model):
@@ -10,21 +19,13 @@ class RailwayStation(models.Model):
         return f'{self.name}'
 
 
-class Train(models.Model):
-    class Type(models.TextChoices):
-        CARGO = 'CT', _('Cargo Train')
-        PASSENGER = 'PT', _('Passenger Train')
-
-    number = models.CharField(max_length=6, blank=False)
-    type = models.CharField(max_length=2, choices=Type.choices, default=Type.PASSENGER, blank=False)
-
-    def __str__(self):
-        return f'{self.number}, {self.type}'
-
-
 class Route(models.Model):
+    class Meta:
+        ordering = ["name"]
+
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField()
+    stations = models.ManyToManyField(RailwayStation, related_name='routes', blank=True)
 
     def clean(self):
         if not self.name:  # does not have an effect until more complex validation
@@ -36,3 +37,26 @@ class Route(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+class Train(models.Model):
+    class Type(models.TextChoices):
+        CARGO = 'CT', _('Cargo Train')
+        PASSENGER = 'PT', _('Passenger Train')
+
+    number = models.CharField(max_length=6, blank=False)
+    type = models.CharField(max_length=2, choices=Type.choices, default=Type.PASSENGER, blank=False)
+    route = models.ForeignKey(Route, on_delete=models.SET_NULL, null=True, blank=True, related_name='trains')
+    station = models.ForeignKey(RailwayStation, on_delete=models.SET_NULL, null=True, blank=True, related_name='trains')
+
+    def __str__(self):
+        return f'{self.number}, {self.type}'
+
+
+class Ticket(models.Model):
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+    start_station = models.ForeignKey(RailwayStation, on_delete=models.CASCADE, related_name='departure_tickets')
+    end_station = models.ForeignKey(RailwayStation, on_delete=models.CASCADE, related_name='arrival_tickets')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tickets')
+
+    def __str__(self):
+        return f'Ticket for {self.train.number} train from {self.start_station.name} to {self.end_station.name}'
